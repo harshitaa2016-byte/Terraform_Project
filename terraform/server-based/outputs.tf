634215e1c7d04396f5c1a54a6da130d9410c0954
Harshitaa Singh <singhharshita2016@gmail.com>
2025-12-13 01:07:16 +0530

    Update outputs.tf

--- terraform/server-based/outputs.tf
+++ terraform/server-based/outputs.tf
@@ -1,72 +1,24 @@
-
-# VPC Outputs
 output "vpc_id" {
-  description = "ID of the VPC"
-  value       = aws_vpc.main.id
+  value = aws_vpc.main.id
 }
 
-output "vpc_cidr_block" {
-  description = "CIDR block of the VPC"
-  value       = aws_vpc.main.cidr_block
+output "eks_cluster_name" {
+  value = aws_eks_cluster.main.name
 }
 
-# Subnet Outputs
+output "eks_endpoint" {
+  value = aws_eks_cluster.main.endpoint
+}
+
 output "public_subnet_ids" {
-  description = "IDs of the public subnets"
-  value       = aws_subnet.public[*].id
+  value = aws_subnet.public[*].id
 }
 
 output "private_subnet_ids" {
-  description = "IDs of the private subnets (Lambda runs here)"
-  value       = aws_subnet.private[*].id
+  value = aws_subnet.private[*].id
 }
 
-output "public_subnet_cidr_blocks" {
-  description = "CIDR blocks of the public subnets"
-  value       = aws_subnet.public[*].cidr_block
-}
-
-output "private_subnet_cidr_blocks" {
-  description = "CIDR blocks of the private subnets"
-  value       = aws_subnet.private[*].cidr_block
-}
-
-# Network Components
-output "internet_gateway_id" {
-  description = "ID of the Internet Gateway"
-  value       = aws_internet_gateway.main.id
-}
-
-output "lambda_security_group_id" {
-  description = "ID of the Lambda security group"
-  value       = aws_security_group.lambda_sg.id
-}
-
-# Lambda Outputs
-output "lambda_function_name" {
-  description = "Name of the Lambda function"
-  value       = aws_lambda_function.main.function_name
-}
-
-output "lambda_function_arn" {
-  description = "ARN of the Lambda function"
-  value       = aws_lambda_function.main.arn
-}
-
-output "lambda_role_arn" {
-  description = "ARN of the Lambda execution role"
-  value       = aws_iam_role.lambda_role.arn
-}
-
-# API Gateway Outputs
-output "api_gateway_invoke_url" {
-  description = "Invoke URL for the API Gateway"
-  value       = "https://${aws_api_gateway_rest_api.main.id}.execute-api.${var.aws_region}.amazonaws.com/${aws_api_gateway_stage.main.stage_name}/hello"
-}
-
-
-output "api_gateway_rest_api_id" {
-  description = "ID of the API Gateway REST API"
-  value       = aws_api_gateway_rest_api.main.id
+output "alb_dns_name" {
+  value = aws_lb.main.dns_name
 }
 
