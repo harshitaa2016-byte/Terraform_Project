72c950ec75f9008403151c6ece5a70a38d7de5bf
Harshitaa Singh <singhharshita2016@gmail.com>
2025-12-13 01:08:22 +0530

    Update variables.tf

--- terraform/server-based/variables.tf
+++ terraform/server-based/variables.tf
@@ -1,39 +1,13 @@
-variable "aws_region" {
+variable "region" {
   description = "AWS region"
   type        = string
   default     = "us-east-1"
 }
 
-variable "environment" {
-  description = "Environment name"
-  type        = string
-  default     = "dev"
-}
-
-variable "project_name" {
+variable "name" {
   description = "Project name"
   type        = string
-  default     = "simple-serverless"
-}
-
-variable "lambda_runtime" {
-  description = "Lambda runtime"
-  type        = string
-  default     = "python3.9"
-}
-
-variable "lambda_timeout" {
-  description = "Lambda timeout in seconds"
-  type        = number
-  default     = 30
-}
-
-
-
-variable "lambda_memory_size" {
-  description = "Lambda memory size in MB"
-  type        = number
-  default     = 128
+  default     = "eks-app"
 }
 
 variable "azs" {
@@ -41,3 +15,10 @@ variable "azs" {
   type        = list(string)
   default     = ["us-east-1a", "us-east-1b"]
 }
+
+variable "image" {
+  description = "Container image"
+  type        = string
+  default     = "nginx"
+}
+
