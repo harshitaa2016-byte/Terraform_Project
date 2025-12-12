4be2eb2fe5bb020aaa194ee3049ccfb310b55eb7
Harshitaa Singh <singhharshita2016@gmail.com>
2025-12-13 01:07:45 +0530

    Update providers.tf

--- terraform/server-based/providers.tf
+++ terraform/server-based/providers.tf
@@ -6,9 +6,14 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
   }
 }
 
+
 provider "aws" {
-  region = var.aws_region
+  region = var.region
 }
